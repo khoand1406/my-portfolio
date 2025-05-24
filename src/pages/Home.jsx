d8aa705873915d07bbc8460a18a0ba0d5384ed2b
Nguyen Khoa <khoandhe160736@fpt.edu.vn>
2025-05-25 03:21:58 +0700

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home= ()=> {
+    return <div className="min-h-screen container">
+        {/*Theme toggle*/}
+
+        {/*Background effects */}
+
+        {/*Navbar */}
+
+        {/*Main Content */}
+
+        {/*Footer */}
+
+    </div>
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+export const NotFound = ()=> {
+    return <h1 className="text-3xl font-bold text-right text-red-500">
+        Hello world!
+    </h1>
+}
